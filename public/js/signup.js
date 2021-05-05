4f87c8daf4035ce38237e0f79c7326464d526d09
Joe <jmwinitsky@gmail.com>
2021-05-04 21:25:36 -0700

    changed loggedIn to logged_in in handlebars templates

--- public/js/signup.js
+++ public/js/signup.js
@@ -9,7 +9,7 @@ const signupFormHandler = async (event) => {
     if (name && username && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ name, username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
