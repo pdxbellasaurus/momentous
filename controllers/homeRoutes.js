08fc5be329d59615982ec81bc1610396285dd56e
Carson C <carson.carraher@gmail.com>
2021-05-06 20:30:11 -0700

    mad new rout for newevent and newevent.js file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,6 +79,25 @@ router.get('/event/:unique_id', async (req, res) => {
     }
   });
 
+  router.get('/newevent', withAuth, async (req, res) => {
+    try{
+
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [ { model: Event } ],
+      });
+
+      const user = userData.get({ plain: true });
+
+      res.render('newevent', {
+        user: user,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  })
+
   router.get('/login', (req, res) => {
     if (req.session.logged_in) {
       res.redirect('/profile');
---
+++ public/js/newEvent.js
@@ -0,0 +1,1 @@
+//New event form
