6275ac6ad3306def176a58add110650124b80172
Joe <jmwinitsky@gmail.com>
2021-05-06 20:04:31 -0700

    newevent handlebar and form handler

---
+++ public/js/newevent.js
@@ -0,0 +1,25 @@
+const neweventFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-newevent').value.trim();
+    const description = document.querySelector('#description-newevent').value.trim();
+
+  
+    if (title && description) {
+      const response = await fetch('/api/events', {
+        method: 'POST',
+        body: JSON.stringify({ title, description }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  document
+  .querySelector('#newevent-form')
+  .addEventListener('submit', neweventFormHandler);
