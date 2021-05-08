9c039e4c0e0e958161ae16bf7c2744d283ba8298
Joe <jmwinitsky@gmail.com>
2021-05-08 11:01:42 -0700

    commented out a console log and removed unused/commented profile handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -72,7 +72,7 @@ router.get('/event/:unique_id', async (req, res) => {
       });
   
       const user = userData.get({ plain: true });
-      console.log(user)
+      // console.log(user)
   
       res.render('profile', {
         user: user,
